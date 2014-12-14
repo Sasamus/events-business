6a3728e5de854659de729fd9e0ab5eff1c85f6ef
Sasamus <labus93@gmail.com>
2014-12-14 14:44:15 +0100

    Declared DatabaseManager stateless

--- ejbModule/ae/eventsbusinessassignment4/databasemanaging/DatabaseManager.java
+++ ejbModule/ae/eventsbusinessassignment4/databasemanaging/DatabaseManager.java
@@ -1,5 +1,6 @@
 package ae.eventsbusinessassignment4.databasemanaging;
 
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -10,6 +11,7 @@ import javax.persistence.PersistenceUnit;
  * 
  * @author Albin Engström
  */
+@Stateless
 public class DatabaseManager {
 	
 	/**
