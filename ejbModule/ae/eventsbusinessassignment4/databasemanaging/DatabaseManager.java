28ce80134d85504bc0c6e02493a9b6aa49553636
Sasamus <labus93@gmail.com>
2014-12-14 13:56:03 +0100

    Added an EntityManager and it's creation

--- ejbModule/ae/eventsbusinessassignment4/databasemanaging/DatabaseManager.java
+++ ejbModule/ae/eventsbusinessassignment4/databasemanaging/DatabaseManager.java
@@ -1,10 +1,26 @@
 package ae.eventsbusinessassignment4.databasemanaging;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceUnit;
+
 /**
  * Manages the database
  * 
  * @author Albin Engström
  */
 public class DatabaseManager {
+	
+	/**
+	 * An EntitymanagerFactory
+	 */
+	@PersistenceUnit(unitName="EventManagement_alen1200")
+	private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("events");
+	
+	/**
+	 *  An EntityManager
+	 */
+	private static EntityManager entityManager = entityManagerFactory.createEntityManager();
 
 }
