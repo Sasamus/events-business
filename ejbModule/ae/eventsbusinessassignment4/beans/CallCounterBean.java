d9cc95f03b83310430237d1d24572b70fbd824dc
Sasamus <labus93@gmail.com>
2014-12-19 13:13:48 +0100

    Implemented CallCounterBean

--- ejbModule/ae/eventsbusinessassignment4/beans/CallCounterBean.java
+++ ejbModule/ae/eventsbusinessassignment4/beans/CallCounterBean.java
@@ -6,15 +6,72 @@ import javax.ejb.Singleton;
 /**
  * Handles counting calls to show different pages
  */
-@Singleton
+@Singleton(name = "CallCounterBean")
 @LocalBean
 public class CallCounterBean {
 
-    /**
-     * Default constructor. 
-     */
-    public CallCounterBean() {
-        
-    }
+	/**
+	 * Counts the calls to show EventsOverview
+	 */
+	int eventsOverviewCallCounter = 0;
+
+	/**
+	 * Counts the calls to show UserProfile
+	 */
+	int userProfileCallCounter = 0;
+
+	/**
+	 * Counts the calls to show AddEvent
+	 */
+	int addEventCallCounter = 0;
+
+	/**
+	 * Default constructor.
+	 */
+	public CallCounterBean() {
+
+	}
+
+	/**
+	 * Increments eventOverviewCallCounter
+	 */
+	public synchronized void incrementEventsOverviewCallCounter() {
+		eventsOverviewCallCounter++;
+	}
+
+	/**
+	 * Increments addEventsCallCounter
+	 */
+	public synchronized void incrementAddEventCallCounter() {
+		addEventCallCounter++;
+	}
+
+	/**
+	 * Increments userProfileCallCounter
+	 */
+	public synchronized void incrementUserProfileCallCounter() {
+		userProfileCallCounter++;
+	}
+
+	/**
+	 * @return the eventsOverviewCallCounter
+	 */
+	public int getEventsOverviewCallCounter() {
+		return eventsOverviewCallCounter;
+	}
+
+	/**
+	 * @return the userProfileCallCounter
+	 */
+	public int getUserProfileCallCounter() {
+		return userProfileCallCounter;
+	}
+
+	/**
+	 * @return the addEventCallCounter
+	 */
+	public int getAddEventCallCounter() {
+		return addEventCallCounter;
+	}
 
 }
