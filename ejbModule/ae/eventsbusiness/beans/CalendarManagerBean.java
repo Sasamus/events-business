7c5847af04a3da4865d608284807cbb6f960b476
Sasamus <labus93@gmail.com>
2014-12-29 14:34:11 +0100

    Worked out some errors

--- ejbModule/ae/eventsbusiness/beans/CalendarManagerBean.java
+++ ejbModule/ae/eventsbusiness/beans/CalendarManagerBean.java
@@ -4,17 +4,18 @@ import java.io.IOException;
 import java.util.Vector;
 
 import javax.ejb.LocalBean;
-import javax.ejb.Singleton;
+import javax.ejb.Stateless;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson.JacksonFactory;
 import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.model.CalendarListEntry;
 
 /**
  * Manages the Calendars
  */
-@Singleton(name = "CalendarManagerBean")
+@Stateless(name = "CalendarManagerBean")
 @LocalBean
 public class CalendarManagerBean {
 
@@ -56,20 +57,19 @@ public class CalendarManagerBean {
 	/**
 	 * Default constructor.
 	 */
-	public CalendarManagerBean(String clientId, String clientSecret,
-			String refreshToken) {
+	public CalendarManagerBean() {
 
-		this.clientId = clientId;
-		this.clientSecret = clientSecret;
-		this.refreshToken = refreshToken;
+		clientId = "745694979823-hqnj881jlqb0tmfllj0bn8t2bnlo7bhc.apps.googleusercontent.com";
+		clientSecret = "Oavf0Mdlrn0RWMMGghqdo_EU";
+		refreshToken = "1/BnfBDYb17Qz2RjZ157BCFsQrkmcE6kTRSKsuyCwf_msMEudVrK5jSpoR30zcRFq6";
 
 		httpTransport = new NetHttpTransport();
 		jsonFactory = new JacksonFactory();
 
-		calendarService = initCalendar();
+		calendarService = initCalendarService();
 	}
 
-	private Calendar initCalendar() {
+	private Calendar initCalendarService() {
 		GoogleCredential credential = new GoogleCredential.Builder()
 				.setClientSecrets(clientId, clientSecret)
 				.setJsonFactory(jsonFactory).setTransport(httpTransport)
@@ -92,4 +92,19 @@ public class CalendarManagerBean {
 		calendarIds.add(createdCalendar.getId());
 	}
 
+	public void listAllCalendars() throws IOException {
+		com.google.api.services.calendar.model.CalendarList calendarList = calendarService.calendarList().list()
+				.execute();
+
+		System.out.println("Debug:");
+		for (CalendarListEntry calendarListEntry : calendarList.getItems())
+			System.out.println(calendarListEntry.getSummary());
+	}
+
+	public void deleteAllCalendars() throws IOException {
+		for(String id : calendarIds){
+			calendarService.calendars().delete(id).execute();
+		}
+		
+	}
 }
