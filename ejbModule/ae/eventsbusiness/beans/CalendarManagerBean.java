ae3afe70cf8672aca9a094b396edf54a942b0482
Sasamus <labus93@gmail.com>
2014-12-30 11:53:29 +0100

    Added comments

--- ejbModule/ae/eventsbusiness/beans/CalendarManagerBean.java
+++ ejbModule/ae/eventsbusiness/beans/CalendarManagerBean.java
@@ -22,7 +22,7 @@ public class CalendarManagerBean {
 	/**
 	 * Calendars
 	 */
-	private Calendar calendarService = null;
+	private Calendar calendars = null;
 
 	/**
 	 * Calendar Ids
@@ -32,17 +32,17 @@ public class CalendarManagerBean {
 	/**
 	 * The Client Id
 	 */
-	private final String clientId;
+	private final String CLIENT_ID = "745694979823-hqnj881jlqb0tmfllj0bn8t2bnlo7bhc.apps.googleusercontent.com";
 
 	/**
 	 * The Clients Secret
 	 */
-	private final String clientSecret;
+	private final String CLIENT_SECRET = "Oavf0Mdlrn0RWMMGghqdo_EU";
 
 	/**
 	 * The Refresh Token
 	 */
-	private final String refreshToken;
+	private final String REFRESH_TOKEN = "1/BnfBDYb17Qz2RjZ157BCFsQrkmcE6kTRSKsuyCwf_msMEudVrK5jSpoR30zcRFq6";
 
 	/**
 	 * A NetHttpTransport variable
@@ -58,52 +58,86 @@ public class CalendarManagerBean {
 	 * Default constructor.
 	 */
 	public CalendarManagerBean() {
-
-		clientId = "745694979823-hqnj881jlqb0tmfllj0bn8t2bnlo7bhc.apps.googleusercontent.com";
-		clientSecret = "Oavf0Mdlrn0RWMMGghqdo_EU";
-		refreshToken = "1/BnfBDYb17Qz2RjZ157BCFsQrkmcE6kTRSKsuyCwf_msMEudVrK5jSpoR30zcRFq6";
-
+		
+		// Create a NetHttpTransport object
 		httpTransport = new NetHttpTransport();
+		
+		// Create a JacksonFactory object 
 		jsonFactory = new JacksonFactory();
-
-		calendarService = initCalendarService();
+		
+		// Initialize calendars
+		calendars = initCalendarService();
 	}
 
+	/**
+	 * Initializes calendars
+	 * 
+	 * @return A Calendar object
+	 */
 	private Calendar initCalendarService() {
+		
+		// Create a GoogleCredential object
 		GoogleCredential credential = new GoogleCredential.Builder()
-				.setClientSecrets(clientId, clientSecret)
+				.setClientSecrets(CLIENT_ID, CLIENT_SECRET)
 				.setJsonFactory(jsonFactory).setTransport(httpTransport)
-				.build().setRefreshToken(refreshToken);
-
+				.build().setRefreshToken(REFRESH_TOKEN);
+		
+		// Create and return a Calendar object
 		return new Calendar.Builder(httpTransport, jsonFactory, credential)
 				.build();
 	}
 
+	/**
+	 * Create a model.Calendar object
+	 * 
+	 * @throws IOException
+	 */
 	public void createCalendar() throws IOException {
-
+		
+		// Initialize calendar
 		com.google.api.services.calendar.model.Calendar calendar = new com.google.api.services.calendar.model.Calendar();
-
+		
+		// TODO: Change to be dynamic
 		calendar.setSummary("testCalendar");
 		calendar.setTimeZone("America/Los_Angeles");
 
-		com.google.api.services.calendar.model.Calendar createdCalendar = calendarService
+		// Insert calendar to calendars
+		com.google.api.services.calendar.model.Calendar createdCalendar = calendars
 				.calendars().insert(calendar).execute();
-
+		
+		// Add the id of calendar to calendarIds
 		calendarIds.add(createdCalendar.getId());
 	}
 
+	/**
+	 * Lists all Calendars
+	 * 
+	 * @throws IOException
+	 */
 	public void listAllCalendars() throws IOException {
-		com.google.api.services.calendar.model.CalendarList calendarList = calendarService.calendarList().list()
+		
+		// Initialize calendarList
+		com.google.api.services.calendar.model.CalendarList calendarList = calendars.calendarList().list()
 				.execute();
-
+		
+		// TODO: Adapt to what it should actually do, below is just testing
 		System.out.println("Debug:");
 		for (CalendarListEntry calendarListEntry : calendarList.getItems())
 			System.out.println(calendarListEntry.getSummary());
 	}
 
+	/**
+	 * Deletes all Calendars
+	 * 
+	 * @throws IOException
+	 */
 	public void deleteAllCalendars() throws IOException {
+		
+		// Iterate through all Ids in calendarIds
 		for(String id : calendarIds){
-			calendarService.calendars().delete(id).execute();
+			
+			// Delete the Calendar with id
+			calendars.calendars().delete(id).execute();
 		}
 		
 	}
